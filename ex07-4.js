55d3c02729eee8af37579f49e3fe60069d59b1bc
zm17488 <zm17488@josai.ac.jp>
2018-12-16 20:59:15 -0500

    message

---
+++ ex07-4.js
@@ -0,0 +1,5 @@
+function add(){
+ var x = document.getElementById('input3').value;
+ var y = document.getElementById('input4').value;
+ alert(Number(x) / Number(y)*Number(y));
+}
--- ex07-8.js
+++ ex07-8.js
@@ -1,7 +1,7 @@
-function nazo(){
- if (document.getElementById('answer').value=='夢'){
-   alert('あたり！');
+function nazo2(){
+ if (document.getElementById('answer2').value=='夢'){
+   alert('あたり!');
  } else {
-   alert('はずれ！');
+   alert('はずれ!');
  }
 }
